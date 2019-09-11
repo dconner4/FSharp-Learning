c67ea2a2a8c0e07edaac3510f5a3f98256b40cc9
dconner4 <danielconner4@gmail.com>
2019-09-10 21:34:06 -0400

    queen-attack exercise

---
+++ queen-attack/QueenAttack.fs
@@ -0,0 +1,8 @@
+﻿module QueenAttack
+open System
+let create ((posX, posY): int * int): bool = 
+    (posX >= 0 && posY >= 0) && (posX < 8 && posY < 8) 
+
+
+let canAttack ((queen1X, queen1Y): int * int) ((queen2X, queen2Y): int * int): bool = 
+    queen1X = queen2X || queen1Y = queen2Y || Math.Abs(queen1X - queen2X) = Math.Abs(queen1Y - queen2Y)
